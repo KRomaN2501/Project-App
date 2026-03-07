ce8f6ca13afb4ee5ef0135a651e43ac68a02ffb4
feuvrt <260897031+feuvrt@users.noreply.github.com>
2026-03-07 23:27:08 +0700

    теперь есть вывод в консоль

--- функции для полей ввода.js
+++ функции для полей ввода.js
@@ -1,4 +1,4 @@
-function Input(logic /* тут будет тип данных по типу ¬арЅлок и тд*/, domElement) {
+function InputAreaOnBlocks(logic /* тут будет тип данных по типу ¬арЅлок и тд*/, domElement) {
     const inputs = domElement.querySelectorAll('.block-input');
 
     inputs.forEach((input, index) => {
@@ -6,7 +6,6 @@ function Input(logic /* тут будет тип данных по типу ¬арЅлок и тд*/, domElement)
             const text = input.value;
 
             if (logic instanceof CreateVarBlock) {
-                //вызов ваших крутых функций 
                 //setNames();
             }
             else if (logic instanceof AssignmentVarBlock) {
@@ -15,12 +14,25 @@ function Input(logic /* тут будет тип данных по типу ¬арЅлок и тд*/, domElement)
                 if (index === 1)
                    //setValues();
             }
+
+            else if (logic instanceof CreateArrBlock) {
+                //setNames();
+            }
+            else if (logic instanceof AssignmentVarBlock) {
+                if (index === 0)
+                    //setNames();
+                if (index === 1)
+                    //setIndex();
+                if (index === 2)
+                    //setValues();
+
+            }
             else if (logic.setCondition) {
-                //setConditions();
+                //setConditions(); - это и дл€ циклов и дл€ условиц
             }
 
             if (logic instanceof PrintVarBlock) {
-                logic.setVarName(text);
+                logic.setNames(text);
             }
 
             else if (logic instanceof PrintArrBlock) {
@@ -32,5 +44,48 @@ function Input(logic /* тут будет тип данных по типу ¬арЅлок и тд*/, domElement)
         });
     });
 }
-
+//вызов функции: 
 Input(blockLogic, newBlock);
+
+
+
+function InputAreaOnConsole() {
+    const inputField = document.getElementById('input-field');
+
+    if (!inputField) return;
+
+    inputField.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            if (!e.shiftKey) {
+                e.preventDefault();
+                const currentText = inputField.value;
+
+                //тут должна быть передача данных дальше куда-нибудь 
+                return currentText;
+
+            }
+        }
+    });
+}
+
+//вызов (мб сразу при запуске или потом где-то вызвать)
+document.addEventListener('ConsoleContent', InputAreaOnSidebar);
+
+
+function printToConsole(txt) {
+    const outputField = document.getElementById('output-field');
+
+    if (outputField) {
+        outputField.value += txt + "\n";
+        outputField.scrollTop = outputField.scrollHeight;
+    }
+}
+
+
+function clearOutput() {
+    const outputField = document.getElementById('output-field');
+
+    if (outputField) {
+        outputField.value = "";
+    }
+}
